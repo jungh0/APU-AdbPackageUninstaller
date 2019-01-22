9468b467a81aaa024bd359a49aa1776618c79473
iveinvalue <iveinvalue@gmail.com>
2019-01-22 17:58:05 +0900

    init

---
+++ Adb Package Uninstaller/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Adb_Package_Uninstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.appl = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.uninstall = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.appl2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Search package list";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(672, 330);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(300, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(701, 109);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(309, 125);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(686, 290);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(8, 49);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(576, 280);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 336);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Selcected apk application-label  : ";
+            // 
+            // appl
+            // 
+            this.appl.AutoSize = true;
+            this.appl.Location = new System.Drawing.Point(202, 336);
+            this.appl.Name = "appl";
+            this.appl.Size = new System.Drawing.Size(27, 12);
+            this.appl.TabIndex = 6;
+            this.appl.Text = " ---";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.appl2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.uninstall);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.appl);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(594, 410);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Package List";
+            // 
+            // uninstall
+            // 
+            this.uninstall.Location = new System.Drawing.Point(394, 375);
+            this.uninstall.Name = "uninstall";
+            this.uninstall.Size = new System.Drawing.Size(190, 23);
+            this.uninstall.TabIndex = 7;
+            this.uninstall.Text = "Uninstall selected package";
+            this.uninstall.UseVisualStyleBackColor = true;
+            this.uninstall.Click += new System.EventHandler(this.uninstall_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 355);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Selcected apk application-name : ";
+            // 
+            // appl2
+            // 
+            this.appl2.AutoSize = true;
+            this.appl2.Location = new System.Drawing.Point(202, 355);
+            this.appl2.Name = "appl2";
+            this.appl2.Size = new System.Drawing.Size(27, 12);
+            this.appl2.TabIndex = 9;
+            this.appl2.Text = " ---";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(299, 382);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(89, 16);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "backup apk";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 434);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "ADB Package Uninstaller (APU)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label appl;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button uninstall;
+        private System.Windows.Forms.Label appl2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Adb Package Uninstaller/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adb_Package_Uninstaller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            uninstall.Enabled = false;
+            checkBox1.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            adb_cmd("adb push aapt-arm-pie /data/local/tmp");
+            adb_cmd("adb shell chmod 0755 /data/local/tmp/aapt-arm-pie");
+            string[] rs = split(adb_cmd("adb shell pm list packages -f"), "package:");
+
+            for (int a = 1; a < rs.Length; a++)
+            {
+                string[] rs2 = split(rs[a], "=");
+                this.listBox1.Items.Add(rs2[0]);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            String temp = (String)listBox1.Items[index];
+            string re3 = adb_cmd("adb shell /data/local/tmp/aapt-arm-pie d badging " + temp);
+            //MessageBox.Show(temp);
+            if (re3.Contains("application-label:"))
+            {
+                string name = split(re3, "application-label:")[1];
+                string name2 = split(name, "\n")[0];
+                //MessageBox.Show(name2);
+                name2 = name2.Replace("'","");
+                appl.Text = name2;
+            }
+            else
+            {
+                appl.Text = "-ERROR-";
+            }
+
+            if (re3.Contains("package: name='"))
+            {
+                string name = split(re3, "package: name='")[1];
+                string name2 = split(name, "'")[0];
+                //MessageBox.Show(name2);
+                appl2.Text = name2;
+            }
+            else
+            {
+                appl2.Text = "-ERROR-";
+            }
+
+            if (appl2.Text.Contains("-ERROR-"))
+            {
+                uninstall.Enabled = false;
+            }
+            else
+            {
+                uninstall.Enabled = true;
+            }
+        }
+
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = adb_cmd(textBox1.Text);
+        }
+
+        public string[] split(string a, string b)
+        {
+            return a.Split(new string[] { b }, StringSplitOptions.None);
+        }
+
+        public string adb_cmd(string str)
+        {
+            ProcessStartInfo cmd = new ProcessStartInfo();
+            Process process = new Process();
+            cmd.FileName = @"cmd";
+            cmd.WindowStyle = ProcessWindowStyle.Hidden;             // cmd창이 숨겨지도록 하기
+            cmd.CreateNoWindow = true;                               // cmd창을 띄우지 안도록 하기
+
+            cmd.UseShellExecute = false;
+            cmd.RedirectStandardOutput = true;        // cmd창에서 데이터를 가져오기
+            cmd.RedirectStandardInput = true;          // cmd창으로 데이터 보내기
+            cmd.RedirectStandardError = true;          // cmd창에서 오류 내용 가져오기
+
+            process.EnableRaisingEvents = false;
+            process.StartInfo = cmd;
+            process.Start();
+            //process.StandardInput.Write(@"chcp 65001" + Environment.NewLine);
+            process.StandardInput.Write(@"cd adb" + Environment.NewLine);
+            process.StandardInput.Write(str + Environment.NewLine);
+            // 명령어를 보낼때는 꼭 마무리를 해줘야 한다. 그래서 마지막에 NewLine가 필요하다
+            process.StandardInput.Close();
+
+            string result = process.StandardOutput.ReadToEnd();
+            //StringBuilder sb = new StringBuilder();
+            //sb.Append("[Result Info]" + DateTime.Now + "\r\n");
+            //sb.Append(result);
+            //sb.Append("\r\n");
+
+            process.WaitForExit();
+            process.Close();
+            return result;
+        }
+
+        private void uninstall_Click(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            String temp = (String)listBox1.Items[index];
+
+
+            //richTextBox1.Text =  adb_cmd("adb pull " + temp + " ../" + appll + ".apk");
+            string result = adb_cmd("adb pull " + temp + " ../" + appl.Text.Replace(" ", "_") + ".apk");
+            if (result.Contains("No such file or directory"))
+            {
+                result = adb_cmd("adb pull " + temp + " ../" + temp.Replace("/", "_") + ".apk");
+            }
+            string r_r = adb_cmd("adb shell pm uninstall -k --user 0 " + appl2.Text);
+            if (r_r.Contains("Success"))
+            {
+                MessageBox.Show("Success Uninstall");
+            }
+            else
+            {
+                MessageBox.Show("Fail Uninstall");
+            }
+
+        }
+    }
+}
